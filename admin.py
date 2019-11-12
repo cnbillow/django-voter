6edc33840828d108832ca7e210f0671fe6de8f1c
Nyanpasi <34001862+Nyanpasi@users.noreply.github.com>
2019-11-13 03:19:16 +0800

    add admin.py

---
+++ admin.py
@@ -0,0 +1,20 @@
+from django.contrib import admin
+from .models import VoteReference
+
+# Register your models here.
+
+
+@admin.register(VoteReference)
+class VoteReferenceAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'user', 
+        'upvote', 
+        'created', 
+        'content_type', 
+        'object_id', 
+        'content_object'
+        )
+
+
+
